10f9e70c95eca7b8a809511dbd1c8306f3d121a5
John Michael <ducnm.john98@gmail.com>
2018-09-07 12:13:36 +0700

    Edit with nodejs

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.post('/', (req, res, next) => {
+  
+})
+
+module.exports = router;
